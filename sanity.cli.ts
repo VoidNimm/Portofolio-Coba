fa133122131a83744d32a03ddcf72a3f8a7d6904
Akmal Ghanim <127508374+VoidNimm@users.noreply.github.com>
2025-08-24 19:37:22 +0700

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = 'YOUR_PROJECT_ID' // Replace with your actual project ID
+const projectId = '74sn78pu' // Replace with your actual project ID
 const dataset = 'production'      // Replace with your actual dataset name
 
 export default defineCliConfig({
